676f26e9795cfbe84aab88fd1d4582820e6fd1d3
atherson <athersonfrank5@gmail.com>
2026-08-03 11:50:47 +0300

    Remove build artifacts and binaries from GitHub tracking

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_hello_FILE /home/atherson/Desktop/Projects/qt/build/hello)
-set(__QT_DEPLOY_TARGET_hello_TYPE EXECUTABLE)
--- build/CMakeFiles/hello.dir/hello_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/hello.dir/hello_autogen/mocs_compilation.cpp.o: \
- /home/atherson/Desktop/Projects/qt/build/hello_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
